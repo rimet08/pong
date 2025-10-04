46216d0dbbf4eee99a6699debfaf851f72cf65af
rimet08 <rmetzner@proton.me>
2025-10-04 22:02:59 +0200

    Tutorial

--- index.cpp
+++ index.cpp
@@ -1,1 +1,17 @@
-// This will be pong game in C++ //
+#include <iostream>
+#include <raylib.h>
+
+using namespace std;
+
+int main() {
+
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+    int ball_x = 100;
+    int ball_y = 100;
+    int ball_speed_x = 5;
+    ins ball_speed_y = 5;
+    int ball_radius = 15;
+
+    cout << "Starting the game..." << endl;
+}
