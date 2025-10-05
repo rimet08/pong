4bf1f56cce9910b40fea842f1357e51c65f12fa6
rimet08 <rmetzner@proton.me>
2025-10-05 19:55:41 +0200

    void Update

--- index.cpp
+++ index.cpp
@@ -29,5 +29,12 @@ class Ball {
         void Draw() {
             DrawCircle(x, y, radius, WHITE);
         }
+
+        void Update() {
+            x += speed_x;
+            y += speed_y;
+
+            if (y + radius >= GetScreenHeight() || y - radius <=0) { speed_y = *= -1;}
+        }
         )
 }
